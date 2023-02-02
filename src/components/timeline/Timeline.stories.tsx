ebf4228d5fb8415b430ac9ea4887dff1fe5373f2
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-02-02 14:36:09 +0100

    feat: add timeline component

---
+++ src/components/timeline/Timeline.stories.tsx
@@ -0,0 +1,134 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Button } from '../button/Button'
+import { Icon } from '../icon/Icon'
+import { Typography } from '../typography/Typography'
+
+import {
+  Timeline,
+  TimelineBody,
+  TimelineConnector,
+  TimelineHeader,
+  TimelineItem,
+  TimelineTitle,
+} from './Timeline'
+import './timeline.scss'
+
+export default {
+  title: 'Components/Timeline',
+  component: Timeline,
+
+  argTypes: {},
+} as ComponentMeta<typeof Timeline>
+
+const Template: ComponentStory<typeof Timeline> = (args) => {
+  const events = [
+    {
+      title: 'Beslut',
+      date: '12 Maj 2023',
+    },
+    {
+      title: 'Komplettera information',
+      date: '12 Maj 2022',
+    },
+    {
+      title: 'Ansökan mottagen',
+      date: '12 Maj 2022',
+    },
+  ]
+
+  return (
+    <Timeline {...args}>
+      {events.map(({ title, date }, i) => (
+        <TimelineItem
+          key={`${title}`}
+          {...(i === 0 ? { active: true } : { muted: true })}
+        >
+          <TimelineConnector />
+          <TimelineBody>
+            <TimelineHeader>
+              <TimelineTitle>{title}</TimelineTitle>
+              <Typography variant='meta'>{date}</Typography>
+            </TimelineHeader>
+          </TimelineBody>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  )
+}
+
+const Template2: ComponentStory<typeof Timeline> = (args) => {
+  type ActionableItems = {
+    url: string
+    text: string
+  }[]
+  const events = [
+    {
+      title: 'Beslut',
+      date: '12 Maj 2023',
+      description:
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      actions: [
+        {
+          url: '#',
+          text: 'Action Link',
+        },
+        {
+          url: '#',
+          text: 'Action Link 2',
+        },
+      ] as ActionableItems,
+    },
+    {
+      title: 'Komplettera information',
+      date: '12 Maj 2022',
+      actions: [] as ActionableItems,
+    },
+    {
+      title: 'Ansökan mottagen',
+      date: '12 Maj 2022',
+      actions: [] as ActionableItems,
+    },
+  ]
+
+  return (
+    <Timeline {...args}>
+      {events.map(({ title, date, description, actions }, i) => (
+        <TimelineItem
+          key={`${title}`}
+          {...(i === 0 ? { active: true } : { muted: true })}
+        >
+          <TimelineConnector />
+          <TimelineBody>
+            <TimelineHeader>
+              <div className='o-grid u-justify-content--center u-align-items--center'>
+                <div className='o-grid-auto'>
+                  <TimelineTitle>{title}</TimelineTitle>
+                  {description ? (
+                    <Typography variant='meta'>{description}</Typography>
+                  ) : null}
+
+                  {actions.map(({ text, url }, i) => (
+                    <Button
+                      key={`${text}-${i}`}
+                      as='anchor'
+                      href={url}
+                      variant='basic'
+                    >
+                      {text}
+                    </Button>
+                  ))}
+                </div>
+                <div className='o-grid-fit u-align-self--start'>
+                  <Typography variant='meta'>{date}</Typography>
+                </div>
+              </div>
+            </TimelineHeader>
+          </TimelineBody>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  )
+}
+
+export const Example = Template.bind({})
+export const Example2 = Template2.bind({})
---
+++ src/components/timeline/Timeline.tsx
@@ -0,0 +1,116 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import { Typography, TypographyProps } from '../typography/Typography'
+
+const Timeline = ({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>): JSX.Element => (
+  <div {...props} className='timeline'>
+    {children}
+  </div>
+)
+
+const TimelineBody = ({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>): JSX.Element => (
+  <div {...props} className='timeline__body'>
+    {children}
+  </div>
+)
+
+const TimelineHeader = ({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>): JSX.Element => (
+  <div {...props} className='timeline__header'>
+    {children}
+  </div>
+)
+
+const TimelineSecondary = ({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>): JSX.Element => (
+  <div {...props} className='timeline__secondary'>
+    {children}
+  </div>
+)
+
+const TimelineTitle = ({
+  children,
+  ...props
+}: TypographyProps): JSX.Element => (
+  <Typography className={'timeline__title'} variant='h4' {...props}>
+    {children}
+  </Typography>
+)
+
+const TimelineLine = (): JSX.Element => <div className='timeline__line'></div>
+
+const TimelineDot = (): JSX.Element => (
+  <div className='timeline__dot'>
+    <div className='timeline__inner-dot'></div>
+  </div>
+)
+
+const TimelineConnector = ({
+  children,
+  ...props
+}: React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>): JSX.Element => (
+  <div {...props} className='timeline__connector'>
+    {children || <TimelineDot />}
+    <TimelineLine />
+  </div>
+)
+
+interface TimelineItemProps extends React.HTMLAttributes<HTMLDivElement> {
+  active?: boolean
+  muted?: boolean
+}
+
+const TimelineItem = ({
+  active,
+  muted,
+  children,
+  ...props
+}: TimelineItemProps): JSX.Element => (
+  <div
+    {...props}
+    className={[
+      'timeline__item',
+      ...(active ? ['is-active'] : []),
+      ...(muted ? ['is-muted'] : []),
+    ].join(' ')}
+  >
+    {children}
+  </div>
+)
+
+export {
+  Timeline,
+  TimelineItem,
+  TimelineConnector,
+  TimelineBody,
+  TimelineDot,
+  TimelineTitle,
+  TimelineSecondary,
+  TimelineLine,
+  TimelineHeader,
+}
