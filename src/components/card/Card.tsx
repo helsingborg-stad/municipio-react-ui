9cb75ba3787b6548dafb3afbb610d554266ea225
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-05-23 10:01:16 +0200

    add card header & footer

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,20 +3,54 @@ import React from 'react'
 
 const Card = ({
   children,
+  className,
   ...props
 }: React.ComponentPropsWithoutRef<'div'>): JSX.Element => (
-  <div {...props} className='c-card'>
+  <div
+    {...props}
+    className={['c-card', ...(className ? [className] : [])].join(' ')}
+  >
     {children}
   </div>
 )
 
 const CardBody = ({
   children,
+  className,
   ...props
 }: React.ComponentPropsWithoutRef<'div'>): JSX.Element => (
-  <div {...props} className='c-card__body'>
+  <div
+    {...props}
+    className={['c-card__body', ...(className ? [className] : [])].join(' ')}
+  >
     {children}
   </div>
 )
 
-export { Card, CardBody }
+const CardHeader = ({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>): JSX.Element => (
+  <div
+    {...props}
+    className={['c-card__header', ...(className ? [className] : [])].join(' ')}
+  >
+    {children}
+  </div>
+)
+
+const CardFooter = ({
+  children,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'div'>): JSX.Element => (
+  <div
+    {...props}
+    className={['c-card__footer', ...(className ? [className] : [])].join(' ')}
+  >
+    {children}
+  </div>
+)
+
+export { Card, CardBody, CardHeader, CardFooter }
